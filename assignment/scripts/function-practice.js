4d83da1a5a0444b0872923b9df6782deb6e5fb66
Mustafa <MustafaGino1@yahoo.com>
2022-12-04 19:50:44 -0600

    done,

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -9,37 +9,48 @@ function hello() {
   return 'Hello World!';
 }
 // Call the function to test
-console.log('Test - should say "Hello World!"', hello());
+console.log( hello());
 
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  console.log("hello my name is", name)
 }
 // Remember to call the function to test
+helloName('Mustafa');
+
 
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
+function addNumbers( firstNumber, secondNumber) {
+  
+  return firstNumber + secondNumber;
   // return firstNumber + secondNumber;
 }
+console.log (addNumbers(99,100));
 
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
+function multiplyThree(num1,num2,num3){
+  return num1 * num2 * num3;
 
 }
+console.log(multiplyThree(3,6,2));
 
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
   if ( number > 0 ){
-    return;
+    return true;
   }
-    return;
+  else 
+    return false;
 }
+
+console.log (isPositive(0));
+console.log (isPositive(10));
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
 console.log( 'isPositive - should say true', isPositive(3) );
@@ -49,17 +60,40 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
-function getLast( array ) {
+
+let cats = ['mila', 'max'];
+
+function getLast( array ){
+array= cats[cats.length-1];
+return array;
+
 
 }
+let empty;
+console.log(' the last item in the array is', getLast());
+console.log('if its empty, it will say', empty);
+
+
+
+
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
+let cars= ['mustang', 'lambo', 'skyline', 'challenger'];
+
 function find( value, array ){
+  for (let x=0; x < cars.length; x++) 
+  if (cars[x]=== value){
+    return true;
+    
+  } else return false;
+  
   
 }
-
+console.log(find('mustang'));
+console.log(find('challenger'));
+console.log(find('camels'));
 // ----------------------
 // Stretch Goals
 // ----------------------
@@ -87,3 +121,11 @@ function sumAll( ) {
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
 //     here in a comment, write the function, and test it!
+
+
+
+
+
+
+
+
